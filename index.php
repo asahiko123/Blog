8d230573fe2e51d7a9e2cc81373b4b694ae6d2b1
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2021-12-25 01:59:23 +0900

    conflict fix

---
+++ index.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head>
+    <link rel="stylesheet" href="css/index.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>引きこもりが燻製チーズ作ったらだめですか？</title>
+</head>
+<body>
+    <header>
+        <p class="logo">
+            <img src="resource/icon-instagram.png">
+        </p>
+        <nav class="gnav">
+            <ul>
+                <li><a href="#">サイト概要</a></li>
+                <li><a href="#">フルハウスとは</a></li>
+                <li><a href="#">新着記事</a></li>
+                <li><a href="#">燻製雑学QUIZ</a></li>
+            </ul>
+        </nav>
+        <div class="dropdown">
+            <button class ="btn_dropdown" id="btn_dropdown">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" width="5em" height="5em" viewBox="0 0 200 200" enable-background="new 0 0 200 200" xml:space="preserve">  <image id="image0" width="200" height="200" x="0" y="0" 
+                xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAMAAACahl6sAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAXVBMVEX////09PTe3t/Q0NHK ysrExMTKysvDw8Ps7OzJycnGxsbIyMjQ0NDFxcXKysnLy8vMzMzPz9D5+fnHx8bHx8fIyMnPz8/r 6+z6+vv6+vrt7e3s7O3f39
+                /R0dHFxcaIgAsFAAAAAWJLR0QAiAUdSAAAAAlwSFlzAAALEQAACxEB f2RfkQAAAAd0SU1FB+UJCwcpL7qUcdYAAAGISURBVHja7dpbbsIwFEVRExIaY0wKhL7b+Q8TkFpV /TVCNrt7jYCj2CT3EYIkSZIkSZIkSZIk6Z4tumU/rB6qWQ39sltcHWOMwzqlWFVK6yGO1+XY5LSd psfKpmmb8u6KGPt4iNVTfGeJ83FfHCTOfe0Av/r5WJpjd2goxyXJU1mOMcfav/2vmJ/LDlZq5H78 
+                mFIsyfEyNPZAzo9keC0I8pabuiEXfX4vCPIxN3ayzmdr7gqCfLZ2RS6XZFMQ5KvFIPk/PxHMHcH8 a2HeI5g3O+dbC/P1y6lHOBUipmYPnC5KwPS1JEmSJEmSJFUF6TRSer+QbjxmPkKZWFFmiJipLmXO jtl8wOyiYLaDMPtabtDdMogbdG1xg66xs+UGHeXrl1OPcCpETM0eOF2UgOlrSZIkSZIkSaoK0mmk 9H4h3XjMfIQysaLMEDFTXcqcHbP5gNlFwWwHYfa13KC7ZRA36NriBl1jZ8sNOsrXL6ce4VSImJo9 
+                cLooAdPXkiRJkiRJkiRJkqS7dAIfPnJofqcBUQAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMS0wOS0x MVQwNzo0MTo0NyswOTowMPK0pCcAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjEtMDktMTFUMDc6NDE6 NDcrMDk6MDCD6RybAAAAAElFTkSuQmCC"/>
+                </svg>  
+            </button>
+            <div class="dropdown_body">
+                <ul>
+                    <li><a class="dropdown_link"href="#">概要</a></li>
+                    <li><a class="dropdown_link"href="#">フルハウスとは</a></li>
+                    <li><a class="dropdown_link"href="#">新着記事</a></li>
+                    <li><a class="dropdown_link"href="#">燻製雑学QUIZ</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="inner">
+        <img src="resource/mainvisual-pc.jpg">
+    </div>
+    <div class="container">
+        <main>
+            <div class="explanation">
+                <h1>燻製の魅力</h1>
+                <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだあーだこーだあーだこーだあーだこーだあーだこーだあーだ
+                    こーだあーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+            </div>
+            <div class="explanation">
+                <h1>新着記事</h1>
+                <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+            </div>
+            <article>
+            
+            <div class="aside">
+                    <h2>記事１</h2>
+                    <img src="resource/works1.jpg">
+                    <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+            </div>
+            <div class="aside">
+                    <h2>記事２</h2>
+                    <img src="resource/works2.jpg">
+                    <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+            </div>
+            <div class="aside">
+                    <h2>記事３</h2>
+                    <img src="resource/works3.jpg">
+                    <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+            </div>
+            <div class="aside">
+                    <h2>記事4</h2>
+                    <img src="resource/works4.jpg">
+                    <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+            </div>
+            </article>
+        </main>
+        <aside>
+            <nav class="bside-nav">
+                <div class="profile">
+                    <h2>プロフィール</h2>
+                    <img src="resource/works2.jpg">
+                    <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+                </div>
+                <div class="bside">
+                    <h2>アーカイブ</h2>
+                    <img src="resource/works3.jpg">
+                    <p>あーだこーだあーだこーだあーだこーだあーだこーだあーだこーだ</p>
+                </div>
+
+            </nav>
+        </aside>
+    </div>
+    <footer>
+        <p>blog_template_By_Hiroki_Asahina</p>
+    <footer>  
+    <script type="text/javascript"src="js/dropdown.js"></script> 
+</body>
+</html>
